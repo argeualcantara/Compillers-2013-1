e6821d2ee200a021eeda4187452a552bb0b5ac8a
a.argeu@gmail.com <a.argeu@gmail.com@68fab6d4-19f7-b8fa-6fdb-b069227401f9>
2013-04-18 22:05:59 +0000

    ajuste no código para adiciona as listas corretamente.

--- trunk/ProjetoCompiladorMiniJava/src/firstFollow/FirstFollow.java
+++ trunk/ProjetoCompiladorMiniJava/src/firstFollow/FirstFollow.java
@@ -1,30 +1,20 @@
 package firstFollow;
 
-import java.util.HashMap;
 import java.util.Iterator;
 
 public class FirstFollow {
-	public static HashMap<String,Terminal> TE = new HashMap<String, Terminal>();
-	public static HashMap<String,String> G = new HashMap<String, String>();
-	public static HashMap<String, NaoTerminal> lista = new HashMap<String, NaoTerminal>();
-	public static void initialize(){
-		G.put("decl-seq", "decl,decl-seq'");
-		G.put("decl-seq'", ";,decl-seq|#");
-		G.put("decl", "s");
-		
-		lista.put("decl-seq", new NaoTerminal("decl-seq"));
-		lista.put("decl-seq'", new NaoTerminal("decl-seq'"));
-		lista.put("decl", new NaoTerminal("decl"));
-		
-		TE.put(";", new Terminal(";"));
-		TE.put("s",  new Terminal(";"));
-	}
-	
 	public static void main(String[] args) {
-		initialize();
 		Gramatica gramatica = new Gramatica();
 		Iterator<String> ite = gramatica.NT.keySet().iterator();
 		NaoTerminal nt = gramatica.NT.get(ite.next());
 		nt.Firsts(gramatica);
+		for (Iterator<String> iterator = gramatica.NT.keySet().iterator(); iterator.hasNext();) {
+			String aux = iterator.next();
+			NaoTerminal naot = gramatica.NT.get(aux);
+			System.out.println("First("+naot.val+"): ");
+			for (Terminal tt : naot.first) {
+				System.out.println(tt.getVal());
+			}
+		}
 	}
 }
