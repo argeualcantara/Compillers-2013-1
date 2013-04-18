4db439ddbc31e497dbd811b69f55434359b89cd8
a.argeu@gmail.com <a.argeu@gmail.com@68fab6d4-19f7-b8fa-6fdb-b069227401f9>
2013-04-18 19:26:52 +0000

    first melhorado

--- trunk/ProjetoCompiladorMiniJava/src/firstFollow/NaoTerminal.java
+++ trunk/ProjetoCompiladorMiniJava/src/firstFollow/NaoTerminal.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class NaoTerminal {
-	public static HashMap<String,Boolean> NT = null;
-	public static HashMap<String,Boolean> TE = null;
-	public static HashMap<String,String> G = null;
+	private static HashMap<String, NaoTerminal> lista = new HashMap<String, NaoTerminal>();
+	public HashMap<String,Boolean> TE = null;
+	public HashMap<String,String> G = null;
 	public String val;
 	public ArrayList<Terminal> first = new ArrayList<Terminal>();
 	public ArrayList<Terminal> follow = new ArrayList<Terminal>();
@@ -18,20 +18,6 @@ public class NaoTerminal {
 	
 	public NaoTerminal(){
 		super();
-		
-		G = new HashMap<String, String>();
-		G.put("decl-seq", "decl,decl-seq'");
-		G.put("decl-seq'", ";,decl-seq|#");
-		G.put("decl", "s");
-		
-		NT = new HashMap<String, Boolean>();
-		NT.put("decl-seq", true);
-		NT.put("decl-seq'", true);
-		NT.put("decl", true);
-		
-		TE = new HashMap<String, Boolean>();
-		TE.put(";", true);
-		TE.put("s", true);
 	}
 	
 	public String getVal() {
@@ -53,28 +39,43 @@ public class NaoTerminal {
 		this.follow = follow;
 	}
 	
-	public Terminal First(HashMap<String, NaoTerminal> lista, int i){
-		String [] prodAux = G.get(this.val).split("\\|");
-		String aux [] = prodAux[i].split(",");
-		if(TE.containsKey(aux[0]) || aux[0].equals("#")){
+	public Terminal First(String valor){
+		String aux [] = valor.split(",");
+		if(TE.containsKey(aux[0])){
 			this.getFirst().add(new Terminal(aux[0]));
-			System.out.println(aux[0]+"\n");
+			System.out.println(aux[0]);
 			return new Terminal(aux[0]);
 		}else{
-			NaoTerminal temp = lista.get(aux[0]);
-			temp.getFirst().add(temp.First(lista, i));
+			NaoTerminal temp = getLista().get(aux[0]);
+			String prodTemp = G.get(temp.val);
+			temp.G = this.G;
+			temp.setLista(this.getLista());
+			temp.TE = this.TE;
+			temp.getFirst().add(temp.First(prodTemp));
 		}
 		return new Terminal("");
 	}
 
-	public static void Firsts(HashMap<String, NaoTerminal> lista) {
-		for(NaoTerminal nt : lista.values()){
+	public void Firsts(Gramatica gramatica) {
+		for(NaoTerminal nt : gramatica.NT.values()){
+			nt.G = gramatica.G;
+			nt.TE = gramatica.TE;
+			nt.setLista(gramatica.NT);
 			System.out.println("FIRST("+nt.val+"):");
-			String aux [] = G.get(nt.val).split("\\|");
+			String aux [] = nt.G.get(nt.val).split("\\|");
 			for (int i = 0; i < aux.length; i++) {
-				nt.First(lista, i);
+				
+				nt.First(aux[i]);
 			}
 		}
 		
+	}
+
+	public HashMap<String, NaoTerminal> getLista() {
+		return lista;
+	}
+
+	public void setLista(HashMap<String, NaoTerminal> lista) {
+		NaoTerminal.lista = lista;
 	}
 }
