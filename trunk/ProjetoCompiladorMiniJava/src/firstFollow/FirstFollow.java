4cbcc7f1c741bd0b27cac9043a75964d527d97fb
a.argeu@gmail.com <a.argeu@gmail.com@68fab6d4-19f7-b8fa-6fdb-b069227401f9>
2013-04-18 23:42:40 +0000

    otimização do código

--- trunk/ProjetoCompiladorMiniJava/src/firstFollow/FirstFollow.java
+++ trunk/ProjetoCompiladorMiniJava/src/firstFollow/FirstFollow.java
@@ -4,13 +4,13 @@ import java.util.Iterator;
 
 public class FirstFollow {
 	public static void main(String[] args) {
-		Gramatica gramatica = new Gramatica();
-		Iterator<String> ite = gramatica.NT.keySet().iterator();
-		NaoTerminal nt = gramatica.NT.get(ite.next());
-		nt.Firsts(gramatica);
-		for (Iterator<String> iterator = gramatica.NT.keySet().iterator(); iterator.hasNext();) {
+		Reader.popularGramatica();
+		Iterator<String> ite = Reader.NAOTERMINAIS.keySet().iterator();
+		NaoTerminal nt = Reader.NAOTERMINAIS.get(ite.next());
+		nt.Firsts();
+		for (Iterator<String> iterator = Reader.NAOTERMINAIS.keySet().iterator(); iterator.hasNext();) {
 			String aux = iterator.next();
-			NaoTerminal naot = gramatica.NT.get(aux);
+			NaoTerminal naot = Reader.NAOTERMINAIS.get(aux);
 			System.out.println("First("+naot.val+"): ");
 			for (Terminal tt : naot.first) {
 				System.out.println(tt.getVal());
--- trunk/ProjetoCompiladorMiniJava/src/firstFollow/NaoTerminal.java
+++ trunk/ProjetoCompiladorMiniJava/src/firstFollow/NaoTerminal.java
@@ -1,14 +1,9 @@
 package firstFollow;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 
 public class NaoTerminal {
-	private static LinkedHashMap<String, NaoTerminal> lista = new LinkedHashMap<String, NaoTerminal>();
-	public HashMap<String,Boolean> TE = null;
-	public LinkedHashMap<String, String> G = null;
 	public String val;
 	public ArrayList<Terminal> first = new ArrayList<Terminal>();
 	public ArrayList<Terminal> follow = new ArrayList<Terminal>();
@@ -43,29 +38,18 @@ public class NaoTerminal {
 	
 	public Terminal First(String valor){
 		String aux [] = valor.split(",");
-		if(TE.containsKey(aux[0])){
-//			this.getFirst().add(new Terminal(aux[0]));
-//			System.out.println(aux[0]);
+		if(Reader.TERMINAIS.containsKey(aux[0])){
 			return new Terminal(aux[0]);
 		}else{
-			NaoTerminal temp = getLista().get(aux[0]);
-			String prodTemp = G.get(temp.val);
-			temp.G = this.G;
-			temp.setLista(this.getLista());
-			temp.TE = this.TE;
-//			temp.getFirst().add(temp.First(prodTemp));
+			NaoTerminal temp = Reader.NAOTERMINAIS.get(aux[0]);
+			String prodTemp = Reader.GRAMATICA.get(temp.val);
 			return temp.First(prodTemp);
 		}
-		//return new Terminal("");
 	}
 
-	public void Firsts(Gramatica gramatica) {
-		for(NaoTerminal nt : gramatica.NT.values()){
-			nt.G = gramatica.G;
-			nt.TE = gramatica.TE;
-			nt.setLista(gramatica.NT);
-//			System.out.println("FIRST("+nt.val+"):");
-			String aux [] = nt.G.get(nt.val).split("\\|");
+	public void Firsts() {
+		for(NaoTerminal nt : Reader.NAOTERMINAIS.values()){
+			String aux [] = Reader.GRAMATICA.get(nt.val).split("\\|");
 			for (int i = 0; i < aux.length; i++) {
 				nt.getFirst().add(nt.First(aux[i]));
 			}
@@ -73,24 +57,16 @@ public class NaoTerminal {
 		
 	}
 	
-	public void Follows(Gramatica gramatica){
-		Iterator<String> ite = gramatica.NT.keySet().iterator();
+	public void Follows(){
+		Iterator<String> ite = Reader.NAOTERMINAIS.keySet().iterator();
 		while(ite.hasNext()){
-			NaoTerminal nt = gramatica.NT.get(ite.next());
-			nt.Follow(gramatica);
+			NaoTerminal nt = Reader.NAOTERMINAIS.get(ite.next());
+			nt.Follow();
 		}
 	}
 
-	private Terminal Follow(Gramatica gramatica) {
+	private Terminal Follow() {
 		
 		return new Terminal("");
-	}
-
-	public LinkedHashMap<String, NaoTerminal> getLista() {
-		return lista;
-	}
-
-	public void setLista(LinkedHashMap<String, NaoTerminal> lista) {
-		NaoTerminal.lista = lista;
 	}
 }
