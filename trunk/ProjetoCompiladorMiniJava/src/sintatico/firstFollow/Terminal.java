c6f942e9673411517af10db3085bb0d81c3e2736
a.argeu@gmail.com <a.argeu@gmail.com@68fab6d4-19f7-b8fa-6fdb-b069227401f9>
2013-04-24 00:40:05 +0000

    reorganização de pacotes

---
+++ trunk/ProjetoCompiladorMiniJava/src/sintatico/firstFollow/Terminal.java
@@ -0,0 +1,19 @@
+package sintatico.firstFollow;
+
+public class Terminal {
+	private String val;
+
+	public Terminal(String val) {
+		super();
+		this.val = val;
+	}
+
+	public String getVal() {
+		return val;
+	}
+
+	public void setVal(String val) {
+		this.val = val;
+	}
+	
+}
